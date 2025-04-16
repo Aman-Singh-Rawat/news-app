e3684988d64e48bcbd443f7e8f58538e95c92cf3
Aman Singh <amanrawat6767@gmail.com>
2025-04-16 17:32:08 +0530

    design forgot_pass

--- lib/screen/auth/forgot_password_screen.dart
+++ lib/screen/auth/forgot_password_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:news_app/main.dart';
+import 'package:news_app/widgets/CustomButton.dart';
+import 'package:news_app/widgets/edit_text_with_title.dart';
+
+import '../../utils/colors.dart';
 
 class ForgotPasswordScreen extends StatefulWidget {
   const ForgotPasswordScreen({super.key});
@@ -10,6 +15,20 @@ class ForgotPasswordScreen extends StatefulWidget {
 }
 
 class _ForgotPasswordScreen extends State<ForgotPasswordScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  bool _isEmailEmpty = true;
+
+  @override
+  void initState() {
+    _emailController.addListener(_isOurTextFieldEmpty);
+    super.initState();
+  }
+
+  void _isOurTextFieldEmpty() {
+    setState(() {
+      _isEmailEmpty = _emailController.text.trim().toString().isEmpty;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,15 +39,28 @@ class _ForgotPasswordScreen extends State<ForgotPasswordScreen> {
           child: Column(
             children: [
               Center(
-                child: SvgPicture.asset(
-                  "assets/images/ic-forgot-password.svg",
-                  width: 250,
+                child: Image.asset(
+                  "assets/images/img-forgot-password.png",
+                  width: 300,
                 ),
               ),
               const SizedBox(height: 10),
               Text(
                 "Select which contact details should we use to reset your password",
-                style: TextStyle(color: Colors.black, fontSize: 12),
+                style: TextStyle(color: Colors.black, fontSize: 13),
+              ),
+              const SizedBox(height: 32),
+              EditTextWithTitle(
+                title: "Email",
+                inputType: TextInputType.emailAddress,
+                isPrimaryColor: true,
+                controller: _emailController,
+              ),
+              const SizedBox(height: 20),
+              customButton(
+                color: _isEmailEmpty ? lightRose : primaryColor,
+                buttonName: "Continue",
+                onClick: () {},
               ),
             ],
           ),
