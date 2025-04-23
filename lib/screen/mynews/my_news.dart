f4a182436a78058dfd3ccce37be601ee0835937c
Aman Singh <amanrawat6767@gmail.com>
2025-04-23 11:59:02 +0530

    add screens

---
+++ lib/screen/mynews/my_news.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class MyNews extends StatelessWidget {
+  const MyNews({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
